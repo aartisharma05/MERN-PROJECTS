79396266cce1f983ec027f6790f00e1ed1211163
AARTI SHARMA <120464099+aartisharma05@users.noreply.github.com>
2023-10-15 23:59:37 +0530

    App.js

---
+++ PROJECT2- Blog- APP/client/src/App.js
@@ -0,0 +1,24 @@
+import "./App.css";
+import Post from "./Post";
+import Header from "./Header";
+import {Route, Routes} from "react-router-dom";
+
+function App() {
+  return (
+    <Routes>
+      <Route
+        index
+        element={
+          <main>
+            <Header />
+            <Post />
+            <Post />
+            <Post />
+          </main>
+        }
+      />
+    </Routes>
+  );
+}
+
+export default App;
