295092ea9d5dc138e1d372caa2e0bfef2d9805eb
AARTI SHARMA <120464099+aartisharma05@users.noreply.github.com>
2023-10-16 23:36:32 +0530

    index.js

---
+++ PROJECT2- Blog- APP/api/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const User = require('./models/User');
+const app = express();
+
+
+app.use(cors());
+
+app.use(express.json());
+
+const MONGODB_URL = 'mongodb+srv://blog:O7kZw0YiNucNGQSO@cluster0.zcswhyw.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose.connect(MONGODB_URL)
+
+app.post('/register',async (req,res)=>{
+   const {username, password} = req.body;
+   const userDoc = await User.create({username, password})
+   res.json(userDoc);
+})
+
+
+app.listen('4000');
+
